5de723ae67b4ea96cb548daa9807d1699cdb93ab
guzval-fab <109773304+guzval-fab@users.noreply.github.com>
2022-07-21 22:46:59 -0600

    Add files via upload

---
+++ proyectouno.js
@@ -0,0 +1,12 @@
+let texto = ['a:', 1, 'b:', 2, 'c:', 3];
+texto.sort(function(a, b) {
+    return a - b;
+  });
+  console.log(texto);
+texto = ['a:','b:','c:']
+console.log(texto)
+let texto2 = texto
+texto2 = [1,2,3]
+console.log(texto2)
+
+
